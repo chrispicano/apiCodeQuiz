9d8521f30726137ec4e94fff7128ff82ae219ecd
chrisPicano <cpicano25@gmail.com>
2021-01-20 18:19:22 -0500

    Added base for timer

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+var intro = document.getElementById("intro");
 var start = document.getElementById("start");
+var choices = document.getElementById("choices");
+var questions = document.getElementById("questions");
 console.log(start)
 start.addEventListener("click", function() {
-    document.getElementById("intro").style.display = "none"
-    document.getElementById("start-quiz").innerHTML = "What does HTML stand for?"
+    intro.style.display = "none"
+    document.getElementById("start-quiz").textContent = questions[0].question;
+    textContent = choices[0];
 })
 
+
+var 
 var questions = 
+
 [
     {
         question: "What does CSS stand for?",
         choices: ["Cascading Style Sheets", "Correcting Sources Systems", "Can't Stand Seinfield"],
         answer: 1
+    },
+
+    {
+        question: "What does HTML stand for?",
+        choices: ["Ham, Turkey, Mayo, Lettuce", "Hypertext Markup Language", "How to meet ladies"],
+        answer: 2
     },
 
     {
@@ -34,27 +47,33 @@ var questions =
 
 
 
+console.log(questions)
 
 
+ for ( var i = 0; i < questions.length; i++ ) {
+     var question = questions[i].question;
+     document.write; ( question );
+ }
+ //timer start
+var currentTime = document.querySelector("#currentTime");
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#start");
+var datadiv = document.querySelector("#datadiv");
+var container = document.querySelector("#container");
+// user starts with 100 seconds 
 
+var secondsLeft = 100;
+var holdInterval = 0;
 
+ if (holdInterval === 0) {
+    holdInterval = setInterval(function () {
+        secondsLeft--;
+        currentTime.textContent = "Time: " + secondsLeft;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-have array of questions w answer
-add timer 
-after test save scores w local storage 
-*/
+        if (secondsLeft <= 0) {
+            clearInterval(holdInterval);
+            alldone();
+            currentTime.textContent = "Sorry time has run out !!!";
+        }
+    }, 1000);
+}
